82155411fae6cb693958771e084f5206522dc562
ElioBoyy <75843356+ElioBoyy@users.noreply.github.com>
2022-06-27 12:25:03 +0200

    map update

---
+++ UnityGame/Assets/LightRandom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRandom : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 startPosition; Vector3 maxDegree; float speed;
+
+    float leRandX;
+    float leRandY;
+    float leRandZ;
+
+    float x;
+    float y;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leRandX = Random.Range(startPosition.x - maxDegree.x, startPosition.x + maxDegree.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startPosition.x >  maxDegree.x)
+        transform.Rotate(startPosition.x * Time.deltaTime, startPosition.y * Time.deltaTime, startPosition.z * Time.deltaTime);
+    }
+}
---
+++ UnityGame/Assets/TransformOnDetect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformOnDetect : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player; float smoothTime = 0.8f; bool isOpened = false;
+
+    float velocity; Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x >= -18 && player.transform.position.x <= -12 && player.transform.position.z <= -39 && player.transform.position.z >= -45)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.SmoothDamp(transform.position.y, 4f, ref velocity, smoothTime), transform.position.z);
+            rb.detectCollisions = false;
+        }
+        else
+            transform.position = new Vector3(transform.position.x, Mathf.SmoothDamp(transform.position.y, 0f, ref velocity, smoothTime), transform.position.z);
+    }
+}
