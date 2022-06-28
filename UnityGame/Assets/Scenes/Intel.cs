8b4ece0f252978991892d6eb79b6b582cc50731a
adkhey746 <97223037+adkhey746@users.noreply.github.com>
2022-06-28 16:24:51 +0200

    IAduZ

---
+++ UnityGame/Assets/Scenes/Intel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Intel : MonoBehaviour
+{
+
+    public Transform player;
+    public NavMeshAgent agent;
+    public float distance;
+    Animator Zombie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Zombie = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, agent.transform.position);
+
+        if (distance <= 5.5 && distance > 1)
+        {
+            Zombie.SetFloat("marcher", 1);
+            agent.SetDestination(player.transform.position);
+        }
+        else if (!Input.GetKey(KeyCode.C) && distance <= 5.5 && distance > 1)
+        {
+
+            agent.SetDestination(player.transform.position);
+        }
+        else if (distance > 5.5)
+        {
+            Zombie.SetFloat("marcher", 0);
+        }
+
+
+    }
+}
---
+++ UnityGame/Assets/Scenes/eclairage.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eclairage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool allumer = false;
+    int range = 50;
+    Light lampe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lampe = GetComponent<Light>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse0) && allumer == false)
+        {
+            lampe.intensity = 1;
+            allumer = true;
+        }
+        else if (Input.GetKey(KeyCode.Mouse1) && allumer == true)
+        {
+            lampe.intensity = 0;
+            allumer = false;
+        }
+
+        lampe.spotAngle = range;
+        if (Input.GetKey(KeyCode.Mouse4))
+        {
+            range--;
+        }
+        else if (Input.GetKey(KeyCode.Mouse3))
+        {
+            range++;
+        }
+        //else if (Input.GetKey(KeyCode.A)&& allumer == true)
+        //{
+        //    lampe.intensity = 0;
+        //    allumer = false;
+        //}
+
+    }
+    //private void MouseWheelHandle(object sender, MouseWheelEventArgs e)
+    //{
+    //    // If the mouse wheel delta is positive, move the box up.
+    //    if (e.Delta > 0)
+    //    {
+    //        lampe.range++;
+    //    }
+
+    //    // If the mouse wheel delta is negative, move the box down.
+    //    if (e.Delta < 0)
+    //    {
+    //        lampe.range--;
+    //    }
+    //}
+}
