662f576ac13f6426d805b2a50fe5e59fa6f2baf6
Nathael007 <108014513+Nathael007@users.noreply.github.com>
2022-06-28 17:47:18 +0200

    Nathael

---
+++ UnityGame/Assets/Prefabs/ImageItem/Script/AfficheComposant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfficheComposant : MonoBehaviour
+{
+    public GameObject composant;
+    float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (composant == null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ UnityGame/Assets/Prefabs/ImageItem/Script/SuppressionItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuppressionItem : MonoBehaviour
+{
+    private GameObject _player1;
+    private GameObject _player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player1 = GameObject.FindGameObjectWithTag("TPSplayer");
+        _player2 = GameObject.FindGameObjectWithTag("FPSplayer");
+
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        float distance1 = Vector3.Distance(this.gameObject.transform.position, _player1.transform.position);
+        float distance2 = Vector3.Distance(this.gameObject.transform.position, _player2.transform.position);
+
+        if (distance1 <= 1.5 || distance2 <= 1.5 && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
